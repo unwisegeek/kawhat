0cf0d0dcb07cc1b3375a21c16a1b5f6a7d8d0dd6
John Madon <john@unwisegeek.net>
2021-01-07 18:40:04 -0500

    Additional handler functionality.

--- handler.php
+++ handler.php
@@ -4,7 +4,7 @@
 session_start();
 $val1 = $_POST["Value1"];
 $val2 = $_POST["Value2"];
-$response = exec('./pingiftt.sh "' . $val1 . '" "' . $val2 '"')
+$response = exec('./pingiftt.sh "' . $val1 . '" "' . $val2 . '"')
 echo '<script />';
 echo 'window.location.replace("https://unwisegeek.net/kawhat/index.html);';
 echo '</script />';
---
+++ pingiftt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X GET --data "value1='$1'&value2='$2'&value3=''" https://maker.ifttt.com/trigger/kawhat_answered/with/key/mn8K4Nyy67NmgErqbBQbGWIHKhSrOzFtcjHb897MPHV
