54161f0f89daa0fd8a4b872e703544331c25a81e
John Madon <john@unwisegeek.net>
2021-01-07 18:10:08 -0500

    Initial Commit

---
+++ handler.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+session_start();
+$val1 = $_POST["Value1"];
+$val2 = $_POST["Value2"];
+$response = exec('./pingiftt.sh "' . $val1 . '" "' . $val2 '"')
+echo '<script />';
+echo 'window.location.replace("https://unwisegeek.net/kawhat/index.html);';
+echo '</script />';
+?>
+</body>
+</html>
