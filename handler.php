d0392f86942ba7cb9cd4f14b42a03c89c3994b29
John Madon <john@unwisegeek>
2021-01-08 00:12:53 -0500

    Changed handler to output results of ifttt.py to /dev/null

--- handler.php
+++ handler.php
@@ -4,7 +4,7 @@
 session_start();
 $val1 = $_POST["Value1"];
 $val2 = $_POST["Value2"];
-$response = exec('./ifttt.py "' . $val1 . '" "' . $val2 . '"');
+$response = exec('./ifttt.py "' . $val1 . '" "' . $val2 . '" > /dev/null');
 echo $response;
 echo '<script />';
 echo 'window.location.replace("https://unwisegeek.net/kawhat/index.php?name='. $val1 . '");';
