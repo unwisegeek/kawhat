5798ab3a4ad514258d7b76cf6291c8ed5b140444
John Madon <john@unwisegeek>
2021-01-08 01:00:55 -0500

    Updated references from Kawhat to Quizlish

--- ifttt.py
+++ ifttt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-# Small script to take two inputs and feed them to an IFTTT event named kawhat_answered
-# By John Madon for the Kawhat Project
+# Small script to take two inputs and feed them to an IFTTT event named quizlish_answers
+# By John Madon for the Quizlish Project
 
 import os
 import sys
@@ -9,23 +9,18 @@ import subprocess
 
 key = ""
 url = ""
-event_id = "kawhat_answered"
+event_id = "quizlish_answers"
 
 if os.path.exists('./key'):
     key = open('./key', 'r').read()
 
-url = "https://maker.ifttt.com/trigger/kawhat_answered/with/key/{}".format(key).strip('\r').strip('\n')
+url = "https://maker.ifttt.com/trigger/{}/with/key/{}".format(event_id, key).strip('\r').strip('\n')
 
 if len(sys.argv) == 3 and len(key) > 0:
    arg1 = sys.argv[1]
    arg2 = sys.argv[2]
    data = '{{"value1":"{}","value2":"{}","value3":""}}'.format(arg1, arg2)
    command = "/usr/bin/curl -X POST -H 'Content-Type: application/json' -d '{}' {}".format(data, url)
-   #print("Command: {}".format(command))
-   #cmd = command.split(' ')
-   #print(cmd)
-   #proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-   #result = proc.communicate()
    result = os.system(command)
    print(result)
 else:
