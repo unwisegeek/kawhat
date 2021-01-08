d3444c6ef3b0345212e118a8ca4ed6902b71e386
John Madon <john@unwisegeek>
2021-01-08 00:45:50 -0500

    Added information about the user's last answer to the index.php page.

--- handler.php
+++ handler.php
@@ -4,10 +4,11 @@
 session_start();
 $val1 = $_POST["Value1"];
 $val2 = $_POST["Value2"];
+$timestamp = $_POST["timestamp"];
 $response = exec('./ifttt.py "' . $val1 . '" "' . $val2 . '" > /dev/null');
 echo $response;
 echo '<script />';
-echo 'window.location.replace("https://unwisegeek.net/kawhat/index.php?name='. $val1 . '");';
+echo 'window.location.replace("https://unwisegeek.net/kawhat/index.php?name='. $val1 . '&last_answer=' . $val2 . '&last_time=' . $timestamp . '");';
 echo '</script />';
 ?>
 </body>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+$br = '<br \>';
+$brbr = $br . $br;
+
 echo '<html \>';
 echo '<head \>';
 echo '<title \>Welcome to Kawhat?</title \>';
@@ -9,19 +13,34 @@ echo '<form action="handler.php" method="post" \>';
 
 if (isset($_GET["name"])) {
   $name = $_GET["name"];
-  echo 'Name: ' . $name . ' <input type="hidden" name="Value1" value="' . $name . '" \><br \><br \>';
+  echo 'Name: ' . $name . ' <input type="hidden" name="Value1" value="' . $name . '" \>' . $brbr;
 } else {
-  echo 'Name: ' . '<input type="text" name="Value1" \><br \><br \>';
+  echo 'Name: ' . '<input type="text" name="Value1" \>' . $brbr;
 }
+date_default_timezone_set("UTC");
+$now_time = date("F j, Y, G:i:s");
+echo "Current Time: " . $now_time . $brbr;
 
 
-echo 'Which is the <em \>false</em \> fact?<br \>';
-echo '<input type="radio" name="Value2" value="1" \> Fact 1<br \>';
-echo '<input type="radio" name="Value2" value="2" \> Fact 2<br \>';
-echo '<input type="radio" name="Value2" value="3" \> Fact 3<br \>';
+echo 'Which is the <em \>false</em \> fact?' . $br;
+echo '<input type="radio" name="Value2" value="1" \> Fact 1' . $br;
+echo '<input type="radio" name="Value2" value="2" \> Fact 2' . $br;
+echo '<input type="radio" name="Value2" value="3" \> Fact 3' . $br;
+echo '<input type="hidden" name="timestamp" value="' . $now_time . '" \>' . $br;
 echo '<input type="submit" \>';
 echo '</form \>';
 echo '</p \>';
+
+if (isset($_GET["last_answer"])) {
+    if (isset($_GET["last_time"])) {
+        $answer = $_GET["last_answer"];
+        $last_time = $_GET["last_time"];
+        echo '<hr \>' . $br;
+        echo 'Last Answer: ' . $answer . $br;
+        echo '  Timestamp: ' . $last_time . $br;
+    }
+}
+
 echo '</body \>';
 echo '</html \>';
 ?>
